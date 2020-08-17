bc749ec76af221ad3c03887a09c94e452bfbea10
EdwinQA <edanwitunry@gmail.com>
2020-08-16 21:29:08 -0500

    version 9
    funcionamiento avances

--- public/app.js
+++ public/app.js
@@ -1,6 +1,3 @@
-
-
-
 var firebaseConfig = {
     apiKey: "AIzaSyBL_LLC4evVeYSpHhYl97uF2jg-hxwAP8I",
     authDomain: "turismo2-4b07d.firebaseapp.com",
@@ -10,6 +7,8 @@ var firebaseConfig = {
     messagingSenderId: "361696427572",
     appId: "1:361696427572:web:93de2285ebce8480679461"
 };
+
+ComprobarInicio = false;
 
 firebase.initializeApp(firebaseConfig);
 
@@ -21,30 +20,119 @@ function ObtenerId(id) {
     return document.getElementById(id).value;
 }
 
-const getSitios = () => db.collection('sitios_turisticos').get();
+const getSitiosVerificados = () => db.collection('sitios_turisticos')
+    .where("estado_validacion", "==", 1).get();
+
+const getTodosSitios = () => db.collection('sitios_turisticos').get();
 
 window.addEventListener('DOMContentLoaded', async (e) => {
-    const querySnapshot = await getSitios();
-    querySnapshot.forEach(doc => {
-        task_sitios.innerHTML += `
-        <div class="card " style="width: 20rem; ">
-        <img class="card-img-top img-fluid" src="img/calera/calera.png" alt="Card image cap">
-        <div class="card-block">
-          <h4 class="card-title">${doc.data().nombre}</h4>
-          <p class="card-text"> UBICACIÓN: ${doc.data().ubicacion} </p>
-          <p class="card-text"> PUNTUACION: ${doc.data().puntuacion} </p>
-          <p class="card-text"> SERVICIOS: ${doc.data().servicios} </p>
-          <p class="card-text">PRESUPUETO: ${doc.data().presupuesto}</p>
-          <a href="#" class="btn btn-outline-primary btn-block">Ver detalles</a>
-          <a href="#" class="btn btn-outline-primary btn-block">Registrar Momento</a>
-        </div>
-      </div>
-    
-        `
-    })
+
+    var user = localStorage['usuarioalmacenado'];
+    if (user) {
+
+        myVar = JSON.parse(user);
+
+        if (myVar.TipoUusario == 0) {
+            console.log('cargando sitios : no administrador');
+            await _mostrarsitiosverificados();
+        } else {
+            console.log('cargando sitios : administrador');
+            await _mostrartodoslossitios();
+        }
+    } else {
+        console.log('cargando sitios : no logueado');
+        await _mostrarsitiosverificados();
+    }
+
 })
 
 
+function VerificarSitio(id) {
+    db.collection("sitios_turisticos").doc(id).update({
+        estado_validacion: 1
+    })
+        .then(function (docRef) {
+            alert('Validacion Exitosa');
+            window.location.href = "index.html";
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function (error) {
+            console.error("Error adding document: ", error);
+        });
+}
+
+async function _mostrarsitiosverificados() {
+    const querySnapshot = await getSitiosVerificados();
+    querySnapshot.forEach(doc => {
+        task_sitios.innerHTML += `
+    <div class="card" style="width: 20rem;">
+    <img class="card-img-top img-fluid" src="img/calera/calera.png" alt="Card image cap">
+    <div class="card-block">
+      <h4 class="card-title">${doc.data().nombre}</h4>
+      <p class="card-text"> UBICACIÓN: ${doc.data().ubicacion} </p>
+      <p class="card-text"> PUNTUACION: ${doc.data().puntuacion} </p>
+      <p class="card-text"> SERVICIOS: ${doc.data().servicios} </p>
+      <p class="card-text">PRESUPUETO: ${doc.data().presupuesto}</p>
+      <a href="#" class="btn btn-outline-primary btn-block">Ver detalles</a>
+      <a href="#" class="btn btn-outline-primary btn-block">Registrar Momento</a>
+    </div>
+  </div>
+    `
+    })
+}
+
+async function _mostrartodoslossitios() {
+
+    const querySnapshot = await getTodosSitios();
+    querySnapshot.forEach(doc => {
+        var botones = ``
+        if (doc.data().estado_validacion) {
+            botones = `
+            <a href="#" class="btn btn-outline-primary btn-block">Ver detalles</a>
+            <a href="#" class="btn btn-outline-primary btn-block">Registrar Momento</a>`
+        } else {
+            botones = `
+            <a 
+            href="#" 
+            class="btn btn-outline-primary btn-block" 
+            onclick="VerificarSitio('${doc.id}')"
+            >Verificar este sitìo </a>`
+        }
+
+        task_sitios.innerHTML += `
+    <div class="card" style="width: 20rem;">
+    <img class="card-img-top img-fluid" src="img/calera/calera.png" alt="Card image cap">
+    <div class="card-block">
+      <h4 class="card-title">${doc.data().nombre}</h4>
+      <p class="card-text"> UBICACIÓN: ${doc.data().ubicacion} </p>
+      <p class="card-text"> PUNTUACION: ${doc.data().puntuacion} </p>
+      <p class="card-text"> SERVICIOS: ${doc.data().servicios} </p>
+      <p class="card-text">PRESUPUETO: ${doc.data().presupuesto}</p>
+      ${botones}
+      </div>
+  </div>
+    `
+    })
+
+}
+
+
+window.addEventListener('DOMContentLoaded', async (e) => {
+    var user = localStorage['usuarioalmacenado'];
+    if (user) {
+        console.log('verificando menu : logueado');
+        myVar = JSON.parse(user);
+        document.getElementById('iniciar_sesion').style.display = 'none';
+        document.getElementById('registrarse').style.display = 'none';
+        document.getElementById('cerrar_sesion').style.display = 'block';
+    } else {
+        console.log('verificando menu : no logueado');
+        document.getElementById('registrarse').style.display = 'block';
+        document.getElementById('iniciar_sesion').style.display = 'block';
+        document.getElementById('cerrar_sesion').style.display = 'none';
+    }
+
+})
 
 
 
@@ -54,19 +142,22 @@ function RegistrarSitio() {
     var Servicios = ObtenerId("servicios");
     var Presupuesto = ObtenerId("presupuesto");
     var Ubicacion = ObtenerId("ubicacion");
-    if (ValidarCamposVaciosSitio(Nombre_sitio,Puntuacion,Servicios,Presupuesto,Ubicacion)) {
-        ColeccionSitio(Nombre_sitio,Puntuacion,Servicios,Presupuesto,Ubicacion);
+    var EstadoValidacion = 0;
+    if (ValidarCamposVaciosSitio(Nombre_sitio, Puntuacion, Servicios,
+        Presupuesto, Ubicacion, EstadoValidacion)) {
+        ColeccionSitio(Nombre_sitio, Puntuacion, Servicios, Presupuesto, Ubicacion, EstadoValidacion);
 
     }
 }
 
-function ColeccionSitio(Nombre_sitio,Puntuacion,Servicios,Presupuesto,Ubicacion) {
+function ColeccionSitio(Nombre_sitio, Puntuacion, Servicios, Presupuesto, Ubicacion, EstadoValidacion) {
     db.collection("sitios_turisticos").doc().set({
         nombre: Nombre_sitio,
         puntuacion: Puntuacion,
         servicios: Servicios,
         presupuesto: Presupuesto,
         ubicacion: Ubicacion,
+        estado_validacion: EstadoValidacion
     })
         .then(function (docRef) {
             alert('Registro Exitoso');
@@ -92,23 +183,52 @@ function Registrar() {
 
     }
 }
-function iniciar(){
-   
-   var user = ObtenerId("usuario")
+
+
+function Menu_Cerrarsesion() {
+    localStorage.removeItem('usuarioalmacenado');
+    window.location.href = "index.html";
+}
+
+function Menu_RegistrarSitio() {
+    var user = localStorage['usuarioalmacenado'];
+    if (user) {
+        console.log('menu registra rsitio : sesion iniciada');
+        window.location.href = "registrarsitio.html";
+    } else {
+        alert('DEBES INICIAR SESION O REGISTRARTE')
+        console.log('menu registrarsitio : Debe registrarse o iniciar sesiòn ');
+    }
+}
+
+
+function iniciar() {
+
+    var user = ObtenerId("usuario")
     var clave = ObtenerId("clave")
     db.collection("usuarios").where("Usuario", "==", user).where("Clave", "==", clave)
-    .get()
-    .then(function(querySnapshot) {
-        querySnapshot.forEach(function(doc) {
-            ComprobarInicio = true;
-           alert('HAZ INICIADO SESION CORRECTAMENTE')
-           window.location.href = "index.html";
-            console.log(doc.id, " => ", doc.data());
+        .get()
+        .then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+                ComprobarInicio = true;
+                alert('HAZ INICIADO SESION CORRECTAMENTE')
+
+                myVar = {
+                    PrimerApellido: doc.data().PrimerApellido,
+                    PrimerNombre: doc.data().PrimerNombre,
+                    SegundoApellido: doc.data().SegundoApellido,
+                    SegundoNombre: doc.data().SegundoNombre,
+                    Telefono: doc.data().Telefono,
+                    Usuario: doc.data().Usuario,
+                    TipoUsuario: doc.data().TipoUsuario,
+                };
+                localStorage['usuarioalmacenado'] = JSON.stringify(myVar);
+                window.location.href = "index.html";
+            });
+        })
+        .catch(function (error) {
+            console.log("Error getting documents: ", error);
         });
-    })
-    .catch(function(error) {
-        console.log("Error getting documents: ", error);
-    });
 }
 
 
@@ -121,7 +241,8 @@ function ColeccionPersona(Nombre_uno, Nombre_dos, Apellido_uno, Apellido_dos, Te
         SegundoApellido: Apellido_dos,
         Telefono: Telefono,
         Usuario: Correo,
-        Clave: Clave
+        Clave: Clave, 
+        TipoUsuario: 0
     })
         .then(function (docRef) {
             alert('Registro Exitoso');
@@ -133,7 +254,7 @@ function ColeccionPersona(Nombre_uno, Nombre_dos, Apellido_uno, Apellido_dos, Te
         });
 }
 
-function ValidarCamposVaciosSitio(Nombre_sitio,Puntuacion,Servicios,Presupuesto,Ubicacion) {
+function ValidarCamposVaciosSitio(Nombre_sitio, Puntuacion, Servicios, Presupuesto, Ubicacion) {
     var Comprobar = true;
     if (Nombre_sitio.length == 0) {
         alert("Debe Ingresar el Nombre del sitio");
@@ -150,7 +271,7 @@ function ValidarCamposVaciosSitio(Nombre_sitio,Puntuacion,Servicios,Presupuesto,
                 if (Presupuesto.length == 0) {
                     alert("Debe ingresar un presupuesto estimado");
                     Comprobar = false;
-                }else{
+                } else {
                     if (Ubicacion.length == 0) {
                         alert("Debe ingresar la ubicacion");
                         Comprobar = false;
